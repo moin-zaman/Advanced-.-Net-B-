4b847508a25d6d216aabe485492f81f010b66156
unknown <moinkhan758abzx@gmail.com>
2022-09-23 16:55:57 +0600

    first commit

---
+++ FirstCV/FirstCV/Controllers/FirstCvController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstCV.Controllers
+{
+    public class FirstCvController : Controller
+    {
+        // GET: FirstCv
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult AboutMe()
+        {
+            return View();
+        } 
+        public ActionResult Education()
+        {
+            return View();
+        } 
+        public ActionResult ContactMe()
+        {
+            return View();
+        }
+
+        public ActionResult WorkSection()
+        {
+            return View();
+        }
+
+        public ActionResult Navbar()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ FirstCV/FirstCV/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FirstCV.MvcApplication" Language="C#" %>
